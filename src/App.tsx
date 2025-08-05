8a003d839579d4f6b1d5762a3f0b4c510956b27e
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-08-05 19:31:24 +0400

    shared client tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import {useEffect} from 'react'
 import './App.css'
+import {client} from "./shared/api/client.ts";
 
 function App() {
     useEffect( () => {
 
-        const response = fetch('https://musicfun.it-incubator.app/api/1.0/playlists', {
-            headers: {
-                'api-key': 'b9b93a14-5751-4e3b-a9c4-4e1503c25972'
-            }
-        }).then(response => response.json())
-        console.log(response)
+        client.GET('/playlists').then( (res)=>{
+            console.log(res.data)
+        } )
+
+        // const response = fetch('https://musicfun.it-incubator.app/api/1.0/playlists', {
+        //     headers: {
+        //         'api-key': 'b9b93a14-5751-4e3b-a9c4-4e1503c25972'
+        //     }
+        // }).then(response => response.json())
+        // console.log(response)
     }, []);
 
   return (
